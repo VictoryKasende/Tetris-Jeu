e7c4a7ab736107b355c1d9a9b97b9c775765af72
VictoryKasende <110433378+VictoryKasende@users.noreply.github.com>
2023-12-29 12:20:26 +0100

    Add files via upload

---
+++ Tetris/src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Ajouter GamePanel a la fenetre
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gp.launchGame();
+
+    }
+}
